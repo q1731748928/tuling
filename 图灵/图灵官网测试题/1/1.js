84b54d715b7326893346c3a865c10efb3c70c7d2
hejie <1731748928@qq.com>
2025-04-16 09:33:22 +0800

    图灵官网测试题提交

--- 图灵/图灵官网测试题/1/1.js
+++
@@ -1,5 +0,0 @@
-document = {}
-e = "Hm_lvt_b5d072258d61ab3cd6a9d485aac7f183"
-"1744635116"
-document.cookie = e + "=" + a + (b.domain ? "; domain=" + b.domain : "") + (b.path ? "; path=" + b.path : "") + (k ? "; expires=" + k.toGMTString() : "") + (b.ec ? "; secure" : "");
-
--- 图灵/图灵官网测试题/4/4.js
+++
@@ -1,5 +0,0 @@
-get_sign = function (pageNumber) {
-    var crypto = require("crypto-js")
-    const timestamp = new Date().getTime()
-    return crypto.MD5("tuling" + timestamp + pageNumber).toString()
-}
